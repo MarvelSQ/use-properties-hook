e2b67a291d75be89be3c5c3bf31f08939e96b532
MarvelSQ <sun1993hlj@hotmail.com>
2019-03-10 22:35:18 +0800

    update: fix package type deine

--- index.d.ts
+++ index.d.ts
@@ -1,24 +1,18 @@
 // In a .d.ts file or .ts file that is not a module:
-declare module '@marvelsq/use-properties-hook' {
-  export function useProperty<P1>(P1: P1): P1;
-  export function useProperties<P1>(P1: P1): [P1];
-  export function useProperties<P1, P2>(P1: P1, P2: P2): [P1, P2];
-  export function useProperties<P1, P2, P3>(
-    P1: P1,
-    P2: P2,
-    P3: P2
-  ): [P1, P2, P3];
-  export function useProperties<P1, P2, P3, P4>(
-    P1: P1,
-    P2: P2,
-    P3: P3,
-    P4: P4
-  ): [P1, P2, P3, P4];
-  export function useProperties<P1, P2, P3, P4, P5>(
-    P1: P1,
-    P2: P2,
-    P3: P3,
-    P4: P4,
-    P5: P5
-  ): [P1, P2, P3, P4, P5];
-}
+export function useProperty<P1>(P1: P1): P1;
+export function useProperties<P1>(P1: P1): [P1];
+export function useProperties<P1, P2>(P1: P1, P2: P2): [P1, P2];
+export function useProperties<P1, P2, P3>(P1: P1, P2: P2, P3: P2): [P1, P2, P3];
+export function useProperties<P1, P2, P3, P4>(
+  P1: P1,
+  P2: P2,
+  P3: P3,
+  P4: P4
+): [P1, P2, P3, P4];
+export function useProperties<P1, P2, P3, P4, P5>(
+  P1: P1,
+  P2: P2,
+  P3: P3,
+  P4: P4,
+  P5: P5
+): [P1, P2, P3, P4, P5];
